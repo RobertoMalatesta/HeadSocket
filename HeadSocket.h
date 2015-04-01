a9a2e1c8c2d78d43eb3306c48f7025cee1f2bc1a
Jan Pinter <Pinter.Jan@gmail.com>
2015-04-01 09:37:36 +0200

    More compact setThreadName

--- HeadSocket.h
+++ HeadSocket.h
@@ -533,25 +533,11 @@ struct DataBlockBuffer
 #ifdef HEADSOCKET_PLATFORM_WINDOWS
 void setThreadName(const char *name)
 {
-  const DWORD MS_VC_EXCEPTION = 0x406D1388;
-
 #pragma pack(push,8)
-  typedef struct tagTHREADNAME_INFO
-  {
-    DWORD dwType; // Must be 0x1000.
-    LPCSTR szName; // Pointer to name (in user addr space).
-    DWORD dwThreadID; // Thread ID (-1=caller thread).
-    DWORD dwFlags; // Reserved for future use, must be zero.
-  } THREADNAME_INFO;
+  typedef struct tagTHREADNAME_INFO { DWORD dwType; LPCSTR szName; DWORD dwThreadID; DWORD dwFlags; } THREADNAME_INFO;
 #pragma pack(pop)
-
-  THREADNAME_INFO info;
-  info.dwType = 0x1000;
-  info.szName = name;
-  info.dwThreadID = -1;
-  info.dwFlags = 0;
-
-  __try { RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info); }
+  THREADNAME_INFO info = { 0x1000, name, -1, 0 };
+  __try { RaiseException(0x406D1388, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info); }
   __except (EXCEPTION_EXECUTE_HANDLER) { }
 }
 #else
